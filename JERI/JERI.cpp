e94bf6b7bad07f67cc0cf9ec1247b70201732372
IDIliev18 <IDIliev18@codingburgas.bg>
2020-10-20 21:27:47 +0300

    Add functions, which gets user's data
    * getUsername()
    * getLevel()

--- JERI/JERI.cpp
+++ JERI/JERI.cpp
@@ -1,20 +1,40 @@
-// JERI.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+string getUsername()
+{
+    fstream userData;
+    string username;
+    userData.open("userData.txt");
+
+    getline(userData, username, '~');
+
+    userData.close();
+
+    return username;
+}
+
+int getLevel()
+{
+    fstream userData;
+    string level[2];
+    userData.open("userData.txt");
+    
+    // Need to be fixed
+    getline(userData, level[0], '~');
+    getline(userData, level[1], '~');
+
+    userData.close();
+
+    return atoi(level[1].c_str());
+}
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const string USERNAME = getUsername();
+    const int LEVEL = getLevel();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
